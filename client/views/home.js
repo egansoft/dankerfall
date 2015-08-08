682001dd73fad58a85dc272d48536c13bdf8ba00
Nicholas Egan <nicholas@egansoft.com>
2015-08-08 00:52:24 -0700

    Fixed display issues

--- client/views/home.js
+++ client/views/home.js
@@ -14,11 +14,14 @@ Template.home.events({
     },
 
     'click #createButton': function() {
-
+        setupGame()
+        Session.set('homepage', 'wait')
     },
 
     'click #enterButton': function() {
-
+        // if(joinGame())
+        //     Session.set('homepage', 'join')
+        console.log(joinGame())
     },
 
     'click .back': function() {
@@ -27,3 +30,22 @@ Template.home.events({
 });
 
 Session.set('homepage', 'home')
+
+var setupGame = function() {
+    var name = $('#newName').val()
+    var hash = Math.random().toString(36).substring(5,10);
+    Games.insert({
+        code: hash,
+        members: [
+            name
+        ]
+    })
+    console.log(Games.find().fetch())
+}
+
+var joinGame = function(code, name) {
+    var name = $('#joinName')
+    var code = $('#joinCode')
+    Session.set('game', Games.findOne({code: code}))
+    return Session.get('game')
+}
